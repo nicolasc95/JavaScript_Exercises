c9992b6333708e35a3f51ac4b18231b14ee16f3c
Nicolás Carducci <nicolasc95@gmail.com>
2020-10-25 17:29:28 -0300

    Exercise 1-a

---
+++ variables_and_operators/app.js
@@ -0,0 +1,15 @@
+
+/* Exercise 1:****************************************************
+Create two numeric variables and use the operator 'sum' to save the
+sum value in both numbers in a third variable.*********************/
+
+var number1 = 4;
+var number2 = 3;
+var number3;
+
+number3 = number1 + number2;
+
+console.log(number3);
+
+
+
