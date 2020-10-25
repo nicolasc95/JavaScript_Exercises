65b472144495d636edb510bbd1c39cd926ba434b
Nicolás Carducci <nicolasc95@gmail.com>
2020-10-25 18:12:16 -0300

    Exercise 2-c

--- strings/app.js
+++ strings/app.js
@@ -17,3 +17,15 @@ var word3 = word2.substring(0,5);
 
 console.log(word3)
 
+/*Exercise C:*******************************************************
+Create a String variable with 10 characters at least and generate a 
+new String with the last 3 characters. Save the result in a new va-
+riable (use substring) ********************************************/
+
+var word4 = 'Hasta la vista, baby';
+
+var word5 = word4.substring(17);
+
+console.log(word5)
+
+
