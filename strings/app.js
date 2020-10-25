440e1cdc49ac4e45bd70871f7d4fd308e1c30f12
Nicolás Carducci <nicolasc95@gmail.com>
2020-10-25 18:51:05 -0300

    Exercise 2-d

--- strings/app.js
+++ strings/app.js
@@ -4,7 +4,7 @@ the text in UpperCase (use toUpperCase).****************************/
 
 var word1 = 'orthopedics shoes';
 
-console.log(word1.toUpperCase())
+console.log(word1.toUpperCase());
 
 /*Exercise B:*******************************************************
 Create a String variable with 10 characters at least and generate a 
@@ -13,9 +13,9 @@ riable (use substring) ********************************************/
 
 var word2 = 'Change it please';
 
-var word3 = word2.substring(0,5);
+var word3 = word2.substr(0,5);
 
-console.log(word3)
+console.log(word3);
 
 /*Exercise C:*******************************************************
 Create a String variable with 10 characters at least and generate a 
@@ -24,8 +24,20 @@ riable (use substring) ********************************************/
 
 var word4 = 'Hasta la vista, baby';
 
-var word5 = word4.substring(17);
+var word5 = word4.substr(-3);
 
-console.log(word5)
+console.log(word5);
 
+/*Exercise C:*******************************************************
+Create a String variable with 10 characters at least and generate a 
+new String with the first letter in uppercase. Save the result in a
+new variable (use substring, toUpperCase, toLowerCase, and + operat-
+or) ***************************************************************/
 
+var word6 = "hello Word How Are You";
+var word7 = word6.substr(0,1)
+var word8 = word6.substr(1);
+
+word9 = word7.toUpperCase() + word8.toLowerCase();
+
+console.log(word9);
