e87f3bae6d4cf461a8134403cd45b11d8a4fc306
Nicolás Carducci <nicolasc95@gmail.com>
2020-10-26 23:26:14 -0300

    Exercise 3-a

---
+++ arrays/app.js
@@ -0,0 +1,10 @@
+/*Exercise A:********************************************************
+Show the 5th and 11th months with the next array:["January","February",
+"March", "April", "May", "June", "July", "August", "September", "Oct-
+ober", "November", "December"](use console.log)**********************/
+
+var months = ["January","February","March", "April", "May", "June", 
+"July", "August", "September", "October", "November", "December"] ;
+
+console.log(months[4] + ' ' + months[10]);
+
