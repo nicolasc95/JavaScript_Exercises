5edffd174cde8695fb6b505e07f4789700940c84
Nicolás Carducci <nicolasc95@gmail.com>
2020-10-27 12:43:19 -0300

    Exercise 5-b

--- week_4/for/app.js
+++ week_4/for/app.js
@@ -5,6 +5,16 @@ Script to show an alert using each word.*****************************/
 var words = ["coffe","coke","limonade","cookies","chocolate"] ;
 var i ;
 
-for(i=0; i < words.length ; i++) {
+for (i=0; i < words.length ; i++) {
   window.alert('Exercise A:' + words[i] ) ;
+}
+
+/*Exercise B:********************************************************
+In the previous Array, transform the first letter of every word in
+UpperCase and show an alert for each modified word*******************/
+
+for (i=0; i < words.length ; i++) {
+  firstLetter = words[i].substr(0,1) ;
+  up = firstLetter.toUpperCase() ;
+  window.alert ('Exercise B:' + ' ' + up + words[i].substr(1) );
 }
