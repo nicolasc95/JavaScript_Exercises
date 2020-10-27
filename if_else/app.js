c38b495a28f3e45c5b1397ef8c4f1333048068e0
Nicolás Carducci <nicolasc95@gmail.com>
2020-10-27 00:11:09 -0300

    Exercise 4-a

---
+++ if_else/app.js
@@ -0,0 +1,7 @@
+/*Exercise A:********************************************************
+Create a random number between 0 and 1 using the function Math.random()
+If the value is bigger or equal than 0.5, show an alert with the mess-
+age "Greater than 0,5". Else, an alert with the message "Lower than 0,5"
+*********************************************************************/
+
+Math.random() >= 0.5 ? console.log('Exercise A: Grater than 0,5') : console.log('Exercise A: Lower than 0,5') ;
